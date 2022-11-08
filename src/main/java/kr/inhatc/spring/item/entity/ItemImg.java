a2923437666b9076ff098a8032eb8350b3e1adbe
Kitae's Github <45898428+kitae104@users.noreply.github.com>
2022-11-08 17:06:43 +0900

    11주차 업데이트

---
+++ src/main/java/kr/inhatc/spring/item/entity/ItemImg.java
@@ -0,0 +1,50 @@
+package kr.inhatc.spring.item.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import kr.inhatc.spring.order.entity.Order;
+import kr.inhatc.spring.utils.entity.BaseEntity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class ItemImg extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "item_img_id")
+    private Long id;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id")
+    private Item item;
+    
+    private String imgName;
+    
+    private String oriName;
+    
+    private String imgUrl;
+    
+    private String repImgYn;
+
+    public void updateItemImg(String oriName, String imgName, String imgUrl) {
+        this.imgName = imgName;
+        this.oriName = oriName;
+        this.imgUrl = imgUrl;
+    }
+    
+    
+}
